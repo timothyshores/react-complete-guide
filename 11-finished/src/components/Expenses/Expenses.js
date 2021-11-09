a46974daf1d70a7c64e2f09891c6fe992b947298
Tim <tim>
2021-11-09 10:15:13 -0700

    Create conditional ternary expression

--- 11-finished/src/components/Expenses/Expenses.js
+++ 11-finished/src/components/Expenses/Expenses.js
@@ -8,15 +8,21 @@ import "./Expenses.css";
 const Expenses = ({ items }) => {
 	const [year, setYear] = useState("2021");
 
+	const filteredExpenses = items.filter(
+		({ date }) => date.getFullYear().toString() === year
+	);
+
 	return (
 		<div>
 			<Card className="expenses">
 				<ExpensesFilter onFilterByYear={(year) => setYear(year)} year={year} />
-				{items
-					.filter(({ date }) => date.getFullYear().toString() === year)
-					.map(({ title, amount, date, id }) => (
+				{filteredExpenses.length === 0 ? (
+					<p>No expenses found</p>
+				) : (
+					filteredExpenses.map(({ title, amount, date, id }) => (
 						<ExpenseItem key={id} title={title} amount={amount} date={date} />
-					))}
+					))
+				)}
 			</Card>
 		</div>
 	);
