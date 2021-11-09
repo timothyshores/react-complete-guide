0f3bd3c2a12bfcdcfb1c7176b8c840f8d382e295
Tim <tim>
2021-11-09 14:36:17 -0700

    Refacot based on instructor's solution

--- 11-finished/src/components/NewExpense/ExpenseForm.js
+++ 11-finished/src/components/NewExpense/ExpenseForm.js
@@ -2,40 +2,32 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = ({ onSubmitExpense }) => {
+const ExpenseForm = ({ onSubmitExpense, toggleFormHidden }) => {
 	const [userInput, setUserInput] = useState({
 		title: "",
 		amount: "",
 		date: "",
 	});
 
-	const [hideInputFields, toggleHideInputField] = useState(true);
-
 	const titleChangeHandler = (e) => {
-		setUserInput((prevState) => {
-			return {
-				...prevState,
-				title: e.target.value,
-			};
-		});
+		setUserInput((prevState) => ({
+			...prevState,
+			title: e.target.value,
+		}));
 	};
 
 	const amountChangeHandler = (e) => {
-		setUserInput((prevState) => {
-			return {
-				...prevState,
-				amount: e.target.value,
-			};
-		});
+		setUserInput((prevState) => ({
+			...prevState,
+			amount: e.target.value,
+		}));
 	};
 
 	const dateChangeHandler = (e) => {
-		setUserInput((prevState) => {
-			return {
-				...prevState,
-				date: e.target.value,
-			};
-		});
+		setUserInput((prevState) => ({
+			...prevState,
+			date: e.target.value,
+		}));
 	};
 
 	const submitHandler = (e) => {
@@ -54,59 +46,39 @@ const ExpenseForm = ({ onSubmitExpense }) => {
 
 	const { title, amount, date } = userInput;
 
-	const handleAddExpense = (e) => {
-		if (Object.values(userInput).every((value) => value === "")) {
-			e.preventDefault();
-			toggleHideInputField((prevState) => !prevState);
-		} else {
-			submitHandler(e);
-		}
-	};
-
-	const handleCancel = (e) => {
-		e.preventDefault();
-		toggleHideInputField((prevState) => !prevState);
-	};
-
 	return (
 		<form onSubmit={submitHandler}>
-			{!hideInputFields && (
-				<div className="new-expense__controls">
-					<div className="new-expense__control">
-						<label>Title</label>
-						<input type="text" value={title} onChange={titleChangeHandler} />
-					</div>
-					<div className="new-expense__control">
-						<label>Amount</label>
-						<input
-							type="number"
-							value={amount}
-							min="0.01"
-							step="0.01"
-							onChange={amountChangeHandler}
-						/>
-					</div>
-					<div className="new-expense__control">
-						<label>Date</label>
-						<input
-							type="date"
-							value={date}
-							min="2019-01-01"
-							max="2031-12-31"
-							onChange={dateChangeHandler}
-						/>
-					</div>
+			<div className="new-expense__controls">
+				<div className="new-expense__control">
+					<label>Title</label>
+					<input type="text" value={title} onChange={titleChangeHandler} />
 				</div>
-			)}
+				<div className="new-expense__control">
+					<label>Amount</label>
+					<input
+						type="number"
+						value={amount}
+						min="0.01"
+						step="0.01"
+						onChange={amountChangeHandler}
+					/>
+				</div>
+				<div className="new-expense__control">
+					<label>Date</label>
+					<input
+						type="date"
+						value={date}
+						min="2019-01-01"
+						max="2031-12-31"
+						onChange={dateChangeHandler}
+					/>
+				</div>
+			</div>
 			<div className="new-expense__actions">
-				{!hideInputFields && (
-					<button type="submit" onClick={handleCancel}>
-						Cancel
-					</button>
-				)}
-				<button type="submit" onClick={handleAddExpense}>
-					Add {hideInputFields && "New"} Expense
+				<button type="button" onClick={() => toggleFormHidden()}>
+					Cancel
 				</button>
+				<button type="submit">Add Expense</button>
 			</div>
 		</form>
 	);
--- 11-finished/src/components/NewExpense/NewExpense.js
+++ 11-finished/src/components/NewExpense/NewExpense.js
@@ -1,12 +1,29 @@
+import React, { useState } from "react";
+
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = ({ onAddExpense }) => {
-	return (
-		<div className="new-expense">
-			<ExpenseForm onSubmitExpense={(data) => onAddExpense({ ...data })} />
-		</div>
+	const [formHidden, toggleFormHidden] = useState(true);
+
+	const handleFormHiddenToggle = (e) => {
+		toggleFormHidden((prevState) => !prevState);
+	};
+
+	let formContent = (
+		<button onClick={handleFormHiddenToggle}>Add New Expense</button>
 	);
+
+	if (!formHidden) {
+		formContent = (
+			<ExpenseForm
+				onSubmitExpense={(data) => onAddExpense({ ...data })}
+				toggleFormHidden={handleFormHiddenToggle}
+			/>
+		);
+	}
+
+	return <div className="new-expense">{formContent}</div>;
 };
 
 export default NewExpense;
